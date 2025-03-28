e0fb8e8e354882322f39eabea8b810e88e80bd0c
Yousef Abdelsalam <yousefeid707@gmail.com>
2025-03-28 19:24:46 -0400

    code compiles but need to fix bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,7 @@ import {
 } from "@/types";
 
 import { initBoard } from "@/utils/inits";
-import { checkResult } from "@/utils";
-
+import { computeUtttBoardResults } from "@/utils";
 
 export default function Game() {
   const [utttBoard, setUtttBoard] = useState(initBoard());
@@ -40,98 +39,43 @@ export default function Game() {
       return;
     }
 
-    function setUtttBoardFn(): UtttBoard {
-      // TODO: Maybe generalize this for some style points?
-      const updatedUtttBoard: UtttBoard = utttBoard.map((array_of_boards: BoardState[], curr_Row) => {
-        if (curr_Row != Row) {
-          return array_of_boards;
+    const updatedUtttBoard: UtttBoard = utttBoard.map((array_of_boards: BoardState[], curr_Row) => {
+      if (curr_Row != Row) {
+        return array_of_boards;
+      }
+
+      return array_of_boards.map((board: BoardState, curr_Col) => {
+        if (curr_Col != Col) {
+          return board;
         }
 
-        return array_of_boards.map((board: BoardState, curr_Col) => {
-          if (curr_Col != Col) {
-            return board;
+        return board.map((board_row: string, curr_row) => {
+          if (curr_row != row) {
+            return board_row;
           }
 
-          return board.map((board_row: string, curr_row) => {
-            if (curr_row != row) {
-              return board_row;
-            }
+          const new_row: string =
+            board_row.substring(0, col) +
+            currentPlayer +
+            board_row.substring(col + 1);
 
-            const new_row: string =
-              board_row.substring(0, col) +
-              currentPlayer +
-              board_row.substring(col + 1);
-
-            return new_row;
-          });
+          return new_row;
         });
       });
-
-      if (checkWin(updatedUtttBoard[Row][Col], currentPlayer)) {
-        console.log(`${currentPlayer} won in board (${Row}, ${Col})`);
-      }
-
-      return updatedUtttBoard;
-    }
-
-    // Click is valid
-    setUtttBoard(setUtttBoardFn);
-
-    // See if board is full
-
-    setBoardResults(bR => {
-      const newbR: BoardResult[][] = bR.map((curr_row, r_idx) => curr_row.map((res, c_idx) => {
-        if (r_idx === Row && c_idx === Col) {
-          const { XWIN, OWIN } = BoardResult;
-
-          // Essentially casting Player into BoardResult
-          return currentPlayer === Player.X ? XWIN : OWIN;
-        }
-        
-        return res;
-      }));
-
-      if (checkResult(newbR, currentPlayer)) {
-        console.log(currentPlayer + " won!");
-      }
-
-      return newbR;
     });
-      
+
+    setUtttBoard(updatedUtttBoard);
+
+    const utttbResults = computeUtttBoardResults(updatedUtttBoard);
+
+    if (utttbResults[Row][Col] === BoardResult.TIE) {
+      console.log(`We have a tie in (${Row}, ${Col})`);
+    } else if (utttbResults[Row][Col] !== BoardResult.UNFINISHED) {
+      console.log(`${utttbResults[Row][Col]} has won on board (${Row}, ${Col})`);
+    }
 
     setCurrentPlayer(turn => (turn === Player.X ? Player.O : Player.X));
-
-    setFocusedBoard(_ => {
-      // Board is already played out
-      if (boardResults[row][col] !== BoardResult.UNFINISHED) {
-        return [-1, -1] as BoardPosition;
-      }
-
-      return [2 - row, col] as BoardPosition;
-    });
-
-    // Check if board is played out now
-    let foundEmpty = true;
-    for (let r = 0; r < utttBoard[Row][Col].length; r++) {
-      for (let c = 0; c < utttBoard[Row][Col][r].length; c++) {
-        if (utttBoard[Row][Col][r][c] === "_") {
-          foundEmpty = true;
-          break;
-        }
-      }
-
-      if (foundEmpty) break;
-    }
-
-    if (!foundEmpty) {
-      setBoardResults(bR => bR.map((curr_row, r_idx) => curr_row.map((res, c_idx) => {
-        if (r_idx === Row && c_idx === Col) {
-          return BoardResult.TIE;
-        }
-
-        return res;
-      })));
-    }
+    setFocusedBoard(utttbResults[row][col] !== BoardResult.UNFINISHED ? [2 - row, col]: [-1, -1]);
   }
   return (
     <div id="home">
